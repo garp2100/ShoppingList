67807bb34c2d946ed6a47b058516566f8eaf4374
Gustavo Rivera <garp2100@gmail.com>
2022-07-17 16:44:06 -0400

    Dictionary and List commit

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -1,1 +1,15 @@
-﻿//My awesome code will go here
+﻿List <int> shoppingList = new ();
+Dictionary<string, double> menuItems = new()
+{
+    { "apples", 2.13},
+    { "yogurt", 3.29 },
+    { "broccoli", 2.15 },
+    { "edelberries", 3.05 },
+    { "blueberries", 4.29 },
+    { "pasta", 5.32 },
+    { "eggs", 4.12 },
+    { "milk", 3.20 },
+};
+
+Console.WriteLine("Welcome to Tropical Market\n");
+Console.WriteLine(menuItems);
