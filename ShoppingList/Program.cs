5317cfa46e162949ffc4510903d1291e1e1c7f68
Gustavo Rivera <garp2100@gmail.com>
2022-07-18 00:52:24 -0400

    whoooooo we're halfway there

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -1,4 +1,6 @@
 ﻿string userInput;
+bool runAgain = true;
+bool wantToContinue = true;
 List <string> shoppingList = new ();
 
 Dictionary<string, decimal> menuItems = new()
@@ -13,6 +15,7 @@ Dictionary<string, decimal> menuItems = new()
     { "milk", 3.20m },
 };
 
+#region Display list to user
 Console.WriteLine("Welcome to Tropical Market\n");
 Console.WriteLine("==========================\n");
 
@@ -20,26 +23,44 @@ foreach (KeyValuePair<string, decimal> item in menuItems)
 {
     Console.WriteLine($"{item.Key}, ${item.Value}");
 }
+#endregion
 
-Console.WriteLine("What item would you to order?:\n");
-userInput = Console.ReadLine();
-if (menuItems.ContainsKey(userInput))
+
+while (wantToContinue)
 {
+    Console.WriteLine("What item would you to order?:\n");
+    userInput = Console.ReadLine();
+    if (menuItems.ContainsKey(userInput))
     {
-        Console.WriteLine("adding " + userInput + " to cart at $" + menuItems[userInput]);
-        shoppingList.Add(userInput);
+        {
+            Console.WriteLine("adding " + userInput + " to cart at $" + menuItems[userInput]);
+            shoppingList.Add(userInput);
+            Console.WriteLine("Would you like to continue? (y/n)?");
+            string restartValue = Console.ReadLine().ToLower();
+            if (restartValue == "y")
+                continue;
+            else
+                break;
+        }
     }
-}
-else
-{
-    Console.WriteLine("Sorry, we don't have those. Please try again.");
+    else
+    {
+        Console.WriteLine("Sorry, we don't have those. Please try again.");
+        Console.WriteLine("Would you like to continue? (y/n)?");
+        string restartValue = Console.ReadLine().ToLower();
+        if (restartValue == "y")
+            continue;
+        else
+            break;
+    } 
 }
 
-foreach (string i in shoppingList)
+
+foreach (string i in shoppingList.ToList())
 {
     if (menuItems.ContainsKey(i))
     {
-        //some loop here
+        shoppingList.Add(i);
     }
+    Console.WriteLine("here's your list: " + i);
 }
-
